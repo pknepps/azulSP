4456e0788336a7a617da6be9a7351c8c9c0a2045
pknepps <pknepps@outlook.com>
2024-05-14 18:55:40 -0400

    started stuff. no clue what I am doing

---
+++ app/ui/gameCenter.tsx
@@ -0,0 +1,11 @@
+
+/**
+ * The center of the game. The center contains all tiles which have yet to be drafted. These tiles
+ * are found in the center of this component and on factories which lie along the circumference
+ * of this component. The center can have any number of factories, and adjusts so that the
+ * factories are always evenly distributed along the circumference.
+ * @param props The elements to place along the radius of the center component.
+ */
+export function useGameCenter(props : any) : void {
+
+}
