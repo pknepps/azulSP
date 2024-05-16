8c35fbccca2396eedbb9cd2c7d103e2ca081103c
pknepps <pknepps@outlook.com>
2024-05-15 22:36:10 -0400

    elements appear in ring after given to GameCenter

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,14 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState, useEffect } from "react";
+import {GameCenter} from "./ui/gameCenter"
+import {ReactElement } from "react";
 
-export default function Home() {
-  const [isClient, setIsClient] = useState(false)
- 
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
-
+export default function Home() : ReactElement | null {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-
+          {GameCenter([1,2,5,1,13,2, 100])}
       </div>
     </main>
   );
--- app/ui/gameCenter.tsx
+++ app/ui/gameCenter.tsx
@@ -1,3 +1,39 @@
+import styles from "./gameCenter.module.css"
+import {ReactElement, ReactNode, useState, useEffect} from "react";
+
+/**
+ * A custom hook which retrieves the dimensions of the current window, with each dimension being
+ * null if no browser instance exists.
+ * @returns an object with 2 number fields: height and width, being the height and width of the
+ *      window respectively.
+ */
+export default function useWindowDimensions() : {width: number | null, height: number | null} {
+    const hasWindow = typeof window !== 'undefined';
+
+    function getWindowDimensions() : {width: number | null, height: number | null} {
+        const width = hasWindow ? window.innerWidth : null;
+        const height = hasWindow ? window.innerHeight : null;
+        return {
+            width,
+            height,
+        };
+    }
+
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        if (hasWindow) {
+            let handleResize = () => {
+                setWindowDimensions(getWindowDimensions());
+            }
+
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }
+    }, [hasWindow, getWindowDimensions]);
+
+    return windowDimensions;
+}
 
 /**
  * The center of the game. The center contains all tiles which have yet to be drafted. These tiles
@@ -5,7 +41,28 @@
  * of this component. The center can have any number of factories, and adjusts so that the
  * factories are always evenly distributed along the circumference.
  * @param props The elements to place along the radius of the center component.
+ * @returns An html element which describes a ring of elements.
  */
-export function useGameCenter(props : any) : void {
-
+export function GameCenter(props : number[]) : ReactElement | null {
+    let cnt = 0;
+    const elements : ReactNode[] = props.map((x : number) : ReactNode => {
+        const angle = cnt++ * (2 * Math.PI) / props.length;
+        return Circle(x, angle);
+    });
+    return (<div className={styles.gameCenter}> {elements} </div>)
 }
+
+/**
+ * Test function
+ */
+export function Circle(num : number, angle : number) : ReactNode {
+    let {width, height} = useWindowDimensions();
+    if (width == null || height == null) {
+        return <div></div>;
+    }
+    const radius = Math.min(height, width) / 3;
+    const verticalOffset = height / 2;
+    const horizontalOffset = width / 2;
+    return <div className={styles.circle} style = {{position : "absolute", right: (Math.cos(angle) * radius + horizontalOffset), top: ((Math.sin(angle) * radius) + verticalOffset)}} key={num}> {num} </div>;
+}
+
